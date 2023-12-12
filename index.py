fd7c8ff0ff0e0c09e8fbbfc0db67c3100dfe729b
Demian <jongho.dev@gmail.com>
2023-12-12 19:16:21 +0900

    update seconds

--- index.py
+++ index.py
@@ -25,10 +25,10 @@ def job(target, is_next_page, is_next_dropbox):
     else:
       print(f'{path_text} {reservation_date_text} 일자에 찾지 못했습니다.')
   
-schedule.every(2).seconds.do(lambda: job('#TD_20231231', False, False)) # 12.31 성판악
-schedule.every(2).seconds.do(lambda: job('#TD_20231231', False, True)) # 12.31 관음사
-schedule.every(2).seconds.do(lambda: job('#TD_20240101', True, False)) # 1.1 성판악
-schedule.every(2).seconds.do(lambda: job('#TD_20240101', True, True)) # 1.1 관음사
+schedule.every(5).seconds.do(lambda: job('#TD_20231231', False, False)) # 12.31 성판악
+schedule.every(5).seconds.do(lambda: job('#TD_20231231', False, True)) # 12.31 관음사
+schedule.every(5).seconds.do(lambda: job('#TD_20240101', True, False)) # 1.1 성판악
+schedule.every(5).seconds.do(lambda: job('#TD_20240101', True, True)) # 1.1 관음사
 
 while True:
     schedule.run_pending()
