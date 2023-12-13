104f399e2dd61938f6da39c2129f39104bf31d48
Demian <jongho.dev@gmail.com>
2023-12-13 10:42:42 +0900

    add try-catch

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import schedule
 from bs4 import BeautifulSoup
 
@@ -7,25 +8,30 @@ from send_to_friend import send_kakao_message_to_friend
 from refresh_token import refresh
 from html_page import fetch_html
 
+sys.stdout.flush()
+
 def job(target, is_next_page, is_next_dropbox):
-  html = fetch_html(is_next_page, is_next_dropbox)
-  soup = BeautifulSoup(html, 'html.parser')
+  try:
+    html = fetch_html(is_next_page, is_next_dropbox)
+    soup = BeautifulSoup(html, 'html.parser')
 
-  available_dates = soup.select(target)
-  for date in available_dates:
-    arr = date.text.strip().split()
-    reservation_date = int(arr[0])
-    reservation_date_text = f'12월 {reservation_date}일' if reservation_date > 10 else f'1월 {reservation_date}일'
-    path_text = '성판악' if is_next_dropbox else '관음사'
-    remain_count = int(arr[2])
+    available_dates = soup.select(target)
+    for date in available_dates:
+      arr = date.text.strip().split()
+      reservation_date = int(arr[0])
+      reservation_date_text = f'12월 {reservation_date}일' if reservation_date > 10 else f'1월 {reservation_date}일'
+      path_text = '성판악' if is_next_dropbox else '관음사'
+      remain_count = int(arr[2])
 
-    if remain_count > 0:
-      text = f'{path_text} {reservation_date_text} 일자에 찾았습니다.\n남은 개수: {remain_count}'
-      send_kakao_message_to_me(text)
-      send_kakao_message_to_friend(text)
-      print(text)
-    else:
-      print(f'{path_text} {reservation_date_text} 일자에 찾지 못했습니다.')
+      if remain_count > 0:
+        text = f'{path_text} {reservation_date_text} 일자에 찾았습니다.\n남은 개수: {remain_count}'
+        send_kakao_message_to_me(text)
+        send_kakao_message_to_friend(text)
+        print(text)
+      else:
+        print(f'{path_text} {reservation_date_text} 일자에 찾지 못했습니다.')
+  except Exception as e:
+    print(f'에러 발생: {e}')
   
 schedule.every(10).seconds.do(lambda: job('#TD_20231231', False, False)) # 12.31 성판악
 schedule.every(10).seconds.do(lambda: job('#TD_20231231', False, True)) # 12.31 관음사
